dd8edf0da7c767858743c24cbbdac79599bd0bee
Turritopsis rubra <87454406+Changhao051@users.noreply.github.com>
2024-05-05 14:25:52 +0800

    Add files via upload

---
+++ Code.txt
@@ -0,0 +1,4 @@
+Supplementary Software 1. Scripts for extracting protein sequences of one-to-one orthologues from each orthogroup.
+Supplementary Software 2. Scripts for individual protein alignments.
+Supplementary Software 3. Scripts for gene loss analyses.
+Supplementary Software 4. Scripts for integrate the datasets corresponding to the medusa.
---
+++ Supplementary Software 1.pl
@@ -0,0 +1,181 @@
+#!/usr/bin/perl -w
+# Copyright (c) BMK 2014/4/30
+# Writer:         tangj <tangj@biomarker.com.cn>
+# Program Date:   2014/4/30.
+# Modifier:       tangj <tangj@biomarker.com.cn>
+# Last Modified:  2014/4/30.
+
+use strict;
+use Cwd;
+use Getopt::Long;
+use Data::Dumper;
+use File::Basename qw(basename dirname);
+use FindBin qw($Bin $Script);
+
+my $programe_dir=basename($0);
+my $path=dirname($0);
+
+my $ver    = "1.0";
+my $Writer = "tangj <tangj\@biomarker.com.cn>";
+my $Data   = "2014/12/09";
+my $BEGIN=time();
+#######################################################################################
+
+# ------------------------------------------------------------------
+# GetOptions
+# ------------------------------------------------------------------
+my ($in,$out,$fa,$deg,$indel,$sin);
+GetOptions(
+			"h|?" =>\&help,
+			"o:s"=>\$out,
+			"i:s"=>\$in,
+			"fa:s"=>\$fa,
+			) || &help;
+&help unless ($in && $out);
+
+sub help
+{
+	print <<"	Usage End.";
+    Description:
+        Writer  : $Writer
+        Data    : $Data
+        Version : $ver
+        function: ......
+    Usage:
+        -i          infile     must be given
+
+        -o          outfile    must be given
+
+        -h          Help document
+	Usage End.
+	exit;
+}
+# ------------------------------------------------------------------
+# GetOptions
+# ------------------------------------------------------------------
+
+###############Time
+my $Time_Start;
+$Time_Start = sub_format_datetime(localtime(time()));
+print "\nStart $programe_dir Time :[$Time_Start]\n\n";
+################
+$/ = "\n";
+open (IN,"$in") || die $!;
+open (FA,"$fa") || die $!;
+
+
+open (OUT,">$out") || die $!;
+my (%hash,%pep);
+$/ = ">";
+<FA>;
+while(<FA>){
+	chomp;
+	my @e = split /\n/,$_,2;
+	my $id = (split /\s+/,$e[0])[0];
+	#print $id;die;
+	$hash{$id} = $e[1];
+}
+$/ = "\n";
+while (<IN>){
+	chomp;
+	my @e = split /\t/,$_;
+	my $file = "Seq2/" . $e[0] . ".fa";
+	#print $file;die;
+	open (OUTT,">$file") || die $!;
+	my $i;
+	for($i=1;$i<@e;$i++){
+		print OUTT ">$e[$i]\n$hash{$e[$i]}";
+	}
+	close OUTT;
+}
+
+
+
+
+
+
+###############Time
+my $Time_End;
+$Time_End = sub_format_datetime(localtime(time()));
+print "\nEnd $programe_dir Time :[$Time_End]\n\n";
+&Runtime($BEGIN);
+
+
+###############Subs
+sub sub_format_datetime #Time calculation subroutine
+{
+	my($sec, $min, $hour, $day, $mon, $year, $wday, $yday, $isdst) = @_;
+	$wday = $yday = $isdst = 0;
+	sprintf("%4d-%02d-%02d %02d:%02d:%02d", $year+1900, $mon+1, $day, $hour, $min, $sec);
+}
+
+sub Runtime # &Runtime($BEGIN);
+{
+	my ($t1)=@_;
+	my $t=time()-$t1;
+	print "Total $programe_dir elapsed time : [",&sub_time($t),"]\n";
+}
+sub sub_time
+{
+	my ($T)=@_;chomp $T;
+	my $s=0;my $m=0;my $h=0;
+	if ($T>=3600) {
+		my $h=int ($T/3600);
+		my $a=$T%3600;
+		if ($a>=60) {
+			my $m=int($a/60);
+			$s=$a%60;
+			$T=$h."h\-".$m."m\-".$s."s";
+		}else{
+			$T=$h."h-"."0m\-".$a."s";
+		}
+	}else{
+		if ($T>=60) {
+			my $m=int($T/60);
+			$s=$T%60;
+			$T=$m."m\-".$s."s";
+		}else{
+			$T=$T."s";
+		}
+	}
+	return ($T);
+}
+
+sub ABSOLUTE_DIR #$pavfile=&ABSOLUTE_DIR($pavfile);
+{
+	my $cur_dir=`pwd`;chomp($cur_dir);
+	my ($in)=@_;
+	my $return="";
+	if(-f $in){
+		my $dir=dirname($in);
+		my $file=basename($in);
+		chdir $dir;$dir=`pwd`;chomp $dir;
+		$return="$dir/$file";
+	}elsif(-d $in){
+		chdir $in;$return=`pwd`;chomp $return;
+	}else{
+		warn "Warning just for file and dir\n";
+		exit;
+	}
+	chdir $cur_dir;
+	return $return;
+}
+sub getSeq	{
+	my $file=shift;
+	my %chr;
+	$/=">";
+	open FA,$file;
+	<FA>;
+	while(<FA>)
+	{
+		chomp;
+		my($head,$seq)=split/\n+/,$_,2;
+		my $id =(split/\s+/,$head)[0];
+		$seq=~s/\n+//g;
+		$seq=~s/>$//;
+		$chr{$id}=$seq;
+	}
+	close FA;
+	$/="\n";
+	return (%chr);
+}
